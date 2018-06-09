0e9c3ec27ca974c50e254f27bce15707da0b227d
craig <craig@notebook>
2018-06-09 11:59:56 -0400

    Added About dialog.

---
+++ ca.footeware.e4.application/src/ca/footeware/e4/application/dialogs/AboutDialog.java
@@ -0,0 +1,69 @@
+/**
+ * 
+ */
+package ca.footeware.e4.application.dialogs;
+
+import java.net.URL;
+
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.layout.GridDataFactory;
+import org.eclipse.jface.layout.GridLayoutFactory;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.resource.JFaceResources;
+import org.eclipse.jface.resource.LocalResourceManager;
+import org.eclipse.jface.resource.ResourceManager;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.FrameworkUtil;
+
+/**
+ * @author Footeware.ca
+ *
+ */
+public class AboutDialog extends Dialog {
+
+	public AboutDialog(Shell parentShell) {
+		super(parentShell);
+		setBlockOnOpen(true);
+	}
+
+	@Override
+	protected Point getInitialSize() {
+		return new Point(450, 400);
+	}
+
+	@Override
+	protected Control createDialogArea(Composite parent) {
+		Composite composite = new Composite(parent, SWT.NONE);
+		GridLayoutFactory.swtDefaults().numColumns(2).equalWidth(false).applyTo(composite);
+		GridDataFactory.fillDefaults().grab(true, true).applyTo(composite);
+
+		ResourceManager resManager = new LocalResourceManager(JFaceResources.getResources(), composite);
+		Bundle bundle = FrameworkUtil.getBundle(this.getClass());
+		// use the org.eclipse.core.runtime.Path as import
+		URL url = FileLocator.find(bundle, new Path("icons/about.png"), null);
+		// get an imageDescriptor and create Image object
+		ImageDescriptor imageDescriptor = ImageDescriptor.createFromURL(url);
+		Image image = resManager.createImage(imageDescriptor);
+		
+		Label imageLabel = new Label(composite, SWT.NONE);
+		imageLabel.setImage(image);
+
+		Text text = new Text(composite, SWT.READ_ONLY);
+		text.setEnabled(true);
+		text.setText("http://footeware.ca");
+		text.setBackground(getShell().getBackground());
+		GridDataFactory.swtDefaults().align(SWT.LEFT, SWT.TOP).applyTo(text);
+		
+		return super.createDialogArea(parent);
+	}
+}
