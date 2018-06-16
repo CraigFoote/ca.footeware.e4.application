e674be188e35cbc760f9824083060def52ef5b68
craig <craig@notebook>
2018-06-16 16:14:51 -0400

    Added link widget to 'about' page.

--- ca.footeware.e4.application/src/ca/footeware/e4/application/dialogs/AboutDialog.java
+++ ca.footeware.e4.application/src/ca/footeware/e4/application/dialogs/AboutDialog.java
@@ -8,6 +8,7 @@ import java.net.URL;
 import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.layout.GridLayoutFactory;
 import org.eclipse.jface.resource.ImageDescriptor;
@@ -17,11 +18,12 @@ import org.eclipse.jface.resource.ResourceManager;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.program.Program;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Link;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.widgets.Text;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.FrameworkUtil;
 
@@ -42,6 +44,11 @@ public class AboutDialog extends Dialog {
 	}
 
 	@Override
+	protected void createButtonsForButtonBar(Composite parent) {
+		createButton(parent, IDialogConstants.OK_ID, IDialogConstants.OK_LABEL, true);
+	}
+
+	@Override
 	protected Control createDialogArea(Composite parent) {
 		Composite composite = new Composite(parent, SWT.NONE);
 		GridLayoutFactory.swtDefaults().numColumns(2).equalWidth(false).applyTo(composite);
@@ -54,16 +61,15 @@ public class AboutDialog extends Dialog {
 		// get an imageDescriptor and create Image object
 		ImageDescriptor imageDescriptor = ImageDescriptor.createFromURL(url);
 		Image image = resManager.createImage(imageDescriptor);
-		
+
 		Label imageLabel = new Label(composite, SWT.NONE);
 		imageLabel.setImage(image);
 
-		Text text = new Text(composite, SWT.READ_ONLY);
-		text.setEnabled(true);
-		text.setText("http://footeware.ca");
-		text.setBackground(getShell().getBackground());
-		GridDataFactory.swtDefaults().align(SWT.LEFT, SWT.TOP).applyTo(text);
-		
+		Link link = new Link(composite, SWT.NONE);
+		link.setText("<a>http://footeware.ca</a>");
+		link.addListener(SWT.Selection, event -> Program.launch(event.text));
+		GridDataFactory.swtDefaults().align(SWT.LEFT, SWT.TOP).applyTo(link);
+
 		return super.createDialogArea(parent);
 	}
 }
