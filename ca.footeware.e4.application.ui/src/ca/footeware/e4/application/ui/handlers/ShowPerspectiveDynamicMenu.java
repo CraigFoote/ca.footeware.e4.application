e4a27cc87ed534d7e17e6564dd2350e8c525d7af
craig <craig@notebook>
2021-03-25 23:32:58 -0400

    Updates

--- ca.footeware.e4.application.ui/src/ca/footeware/e4/application/ui/handlers/ShowPerspectiveDynamicMenu.java
+++ ca.footeware.e4.application.ui/src/ca/footeware/e4/application/ui/handlers/ShowPerspectiveDynamicMenu.java
@@ -3,7 +3,6 @@ package ca.footeware.e4.application.ui.handlers;
 
 import java.util.List;
 
-import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.ui.di.AboutToShow;
 import org.eclipse.e4.ui.model.application.MApplication;
 import org.eclipse.e4.ui.model.application.commands.MCommand;
